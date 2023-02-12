fde25c86bbc62d7789b9e8cac28115b3e1a07401
dlwodnjjs <andrewstar7777@gmail.com>
2023-02-12 11:21:50 +0900

    반복문

---
+++ src/lecture20230212/Lecture04.java
@@ -0,0 +1,13 @@
+package lecture20230212;
+
+public class Lecture04 {
+    public static void main(String[] args) {
+        int sum=0;
+        for(int i=0;i<100;i++){
+            if(i%2==0){
+                sum+=1;
+            }
+        }
+        System.out.println(sum);
+    }
+}
