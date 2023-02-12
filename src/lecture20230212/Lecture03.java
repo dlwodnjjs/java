a57c04c1fdc7afcc6113966eec9f9b3e1a18f6f3
dlwodnjjs <andrewstar7777@gmail.com>
2023-02-12 11:12:53 +0900

    조건문

---
+++ src/lecture20230212/Lecture03.java
@@ -0,0 +1,13 @@
+package lecture20230212;
+
+public class Lecture03 {
+    public static void main(String[] args) {
+        int number=200;
+        if(number==300){
+            System.out.println("300");
+        }
+        else{
+            System.out.println(number);
+        }
+    }
+}
