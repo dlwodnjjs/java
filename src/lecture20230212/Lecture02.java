f21af574f0fe4d18dd50bda78b268600beb78ca5
dlwodnjjs <andrewstar7777@gmail.com>
2023-02-12 11:04:34 +0900

    출력

---
+++ src/lecture20230212/Lecture02.java
@@ -0,0 +1,19 @@
+package lecture20230212;
+
+public class Lecture02 {
+    public static void main(String[] args) {
+        byte number0 = 33;
+        int number1 = 444444444;
+        long number2 = 1L;
+        short number3 = 11111;
+
+        float number4 = 4.0f;
+        double number5 = 4.14;
+        char character = 'm';
+        boolean flag = true;
+        System.out.print(number0);
+        System.out.println(number1);
+        System.out.print(number2);
+        System.out.println(number3);
+    }
+}
